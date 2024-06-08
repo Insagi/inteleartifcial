948048db8c4cc7854b7cefb96941a95e8a070ff4
Insagi <128197337+Insagi@users.noreply.github.com>
2024-06-07 22:29:52 -0300

    s

--- script.js
+++ script.js
@@ -4,13 +4,7 @@ const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
-const perguntas = [
-    enunciado: "Pergunta 1",
-    alternativas: [
-"Alternativa 1",
-"Alternativa 2"
-]
-   ];`
+
 const perguntas = [
     {
         enunciado: "Pergunta 1",
